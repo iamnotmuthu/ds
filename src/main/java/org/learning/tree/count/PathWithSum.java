85b3282032e78b9d6e505fc245cbad1de59024d1
Muthu <iamnotmuthu@gmail.com>
2020-02-11 19:58:02 +0530

    path with given sum

--- src/main/java/org/learning/tree/count/PathWithSum.java
+++ src/main/java/org/learning/tree/count/PathWithSum.java
@@ -1,5 +1,7 @@
 package org.learning.tree.count;
 
+import java.util.Stack;
+
 import org.learning.tree.util.Node;
 import org.learning.tree.util.Tree;
 
@@ -12,11 +14,29 @@ public class PathWithSum {
 
 	public static void main(String[] args) {
 		Node<Integer> root = Tree.getTree();
-		System.out.println(pathSum(root,21));
+		System.out.println(pathSum(root,28,0));
 	}
 
-	private static boolean pathSum(Node<Integer> root, int sum) {
+	private static boolean pathSum(Node<Integer> root, int sum,int tsum) {
+		if(root==null) {
 			return false;
+		}
+		tsum=tsum+root.data;
+		System.out.println("tesmpsum "+tsum);
+		 if( pathSum(root.left,sum,tsum)) {
+			 return true;
+		 }
+		if(root.left==null && root.right==null) {
+			if(tsum==sum) {
+				return true;
+			}
+		} if( pathSum(root.right,sum,tsum)) {
+			return true;
+		}
+		tsum=tsum-root.data;
+		System.out.println("tesmpsum-- "+tsum);
+		return false;
+			
 	}
 
 }
