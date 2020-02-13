0b1b5cd339d2c73d4183e69ca62410ab01049045
Muthu <iamnotmuthu@gmail.com>
2020-02-13 21:09:33 +0530

    BST

---
+++ src/main/java/org/learning/structure/BST.java
@@ -0,0 +1,26 @@
+package org.learning.structure;
+
+import org.learning.tree.util.Node;
+import org.learning.tree.util.Tree;
+
+public class BST {
+
+	public static void main(String[] args) {
+		Node<Integer> root = Tree.getTree();
+		System.out.println(isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE));
+
+	}
+
+	private static boolean isBST(Node<Integer> root, int min, int max) {
+
+		if (root == null)
+			return true;
+		if (root.data <= min || root.data > max) {
+			return false;
+		}
+
+		return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
+
+	}
+
+}
